b25592da6ee7fbc887000db333af75313c1a100a
Dag Hjelm <dag.hjelm@gmail.com>
2023-09-24 18:46:40 +0200

    update link script

--- link.sh
+++ link.sh
@@ -1,4 +1,11 @@
-ln -s zshrc ~/.zshrc
-ln -s vimrc ~/.vimrc
-ln -s vscode_vimrc ~/.vscode_vimrc
-ln -s ideavimrc ~/.ideavimrc
+rm ~/.zshrc 
+rm ~/.vimrc
+rm ~/.vscode_vimrc
+rm ~/.ideavimrc
+rm -rf ~/.config/nvim
+
+ln -s /Users/daghjelm/dotfiles/zshrc ~/.zshrc
+ln -s /Users/daghjelm/dotfiles/vimrc ~/.vimrc
+ln -s /Users/daghjelm/dotfiles/vscode_vimrc  ~/.vscode_vimrc
+ln -s /Users/daghjelm/dotfiles/ideavimrc ~/.ideavimrc
+ln -s /Users/daghjelm/dotfiles/nvim ~/.config/nvim
